9df03a2d3f694f8423c141bd75443a44a914b1a2
KasiaP <katarzynapiechota95@gmail.com>
2017-12-17 22:36:00 +0100

    create graph from txt file

--- projekt_mnd/src/sample/ApplicationController.java
+++ projekt_mnd/src/sample/ApplicationController.java
@@ -1,14 +1,38 @@
 package sample;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
-
+import javafx.scene.control.TextArea;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
 
 public class ApplicationController {
 
     //TODO validate if weight is numeric
+    @FXML private TextArea graphInput;
+    private Graph graph;
 
     @FXML
     protected void add() {
+        this.graph = new SingleGraph("Network");
+        this.graph.setStrict(false);
+        this.graph.setAutoCreate( true );
+
         System.out.println("The button was clicked!");
+        String[] rows = splitInputIntoRows();
+        createGraphFromSingleRows(rows);
+        //here create object which caltulate the shortest/longest paths etc - as an argument get graph
+    }
+
+    public String[] splitInputIntoRows() {
+        String graphInputString = graphInput.getText();
+        String[] rows = graphInputString.split("\n");
+        return rows;
+    }
+
+    public void createGraphFromSingleRows(String[] rows) {
+        for ( String row : rows) {
+            String[] letters = row.split("");
+            this.graph.addEdge( row, letters[0], letters[2] );
+            graph.getEdge(row).setAttribute("weight", Integer.parseInt(letters[1]));
+        }
     }
 }
