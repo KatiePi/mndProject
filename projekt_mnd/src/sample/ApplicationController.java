d371cc2be9ab75e6fc3cdaa0528103830ed14332
KasiaP <katarzynapiechota95@gmail.com>
2017-12-17 21:42:22 +0100

    Create new layout

--- projekt_mnd/src/sample/ApplicationController.java
+++ projekt_mnd/src/sample/ApplicationController.java
@@ -1,4 +1,14 @@
 package sample;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 
 public class ApplicationController {
+
+    //TODO validate if weight is numeric
+
+    @FXML
+    protected void add() {
+        System.out.println("The button was clicked!");
+    }
 }
