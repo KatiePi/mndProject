2828cf98e0bebeb80b9873f4363fc4664b583a9b
Gerwant <mp.rudnicki@gmail.com>
2017-12-31 19:15:11 +0100

    Necessary activities fix

--- projekt_mnd/src/sample/sample/ApplicationController.java
+++ projekt_mnd/src/sample/sample/ApplicationController.java
@@ -1,4 +1,5 @@
 package sample;
+
 import javafx.embed.swing.SwingNode;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
@@ -8,7 +9,6 @@ import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.SingleGraph;
 import org.graphstream.ui.swingViewer.View;
 import org.graphstream.ui.swingViewer.Viewer;
-
 import java.awt.*;
 import java.util.*;
 import java.util.regex.Matcher;
@@ -22,6 +22,10 @@ public class ApplicationController {
     @FXML private TextArea theShortestPathOutput;
     @FXML private TextArea criticalNodesOutput;
     @FXML private Label syntaxErrorLabel;
+    @FXML private Label warning;
+    @FXML private TextArea necessaryNodes;
+    @FXML private Label necessaryNodesWarning;
+    @FXML private TextArea shortestPathWithNecessaryActivitiesOutput;
 
     private Graph graph;
     private GraphProcesser gp;
@@ -30,6 +34,11 @@ public class ApplicationController {
 
     @FXML
     protected void add() {
+        necessaryNodesWarning.setVisible(false);
+        necessaryNodesWarning.setStyle("-fx-text-fill: red;");
+        shortestPathWithNecessaryActivitiesOutput.setText("");
+        warning.setVisible(false);
+        warning.setStyle("-fx-text-fill: red;");
         this.graph = new SingleGraph("Network");
         this.graph.setStrict(false);
         this.graph.setAutoCreate(true);
@@ -39,9 +48,9 @@ public class ApplicationController {
                         "text-padding: 3px, 2px; " +
                         "text-background-mode: rounded-box; " +
                         "text-background-color: #A7CC; " +
-                        "text-color: white; " +
+                        "text-color: black; " +
                         "text-style: bold-italic; " +
-                        "text-offset: 5px, 0px;" +
+                        "text-offset: 5px, 5px;" +
                         "fill-color: black; " +
                         "size: 10px, 10px;" +
                     "}" +
@@ -83,7 +92,6 @@ public class ApplicationController {
 
     public void createGraphFromSingleRows(String[] rows) {
         String shortestPath = "";
-        String longestPath = "";
         String criticalNodesText = "";
         ArrayList<GraphNode> criticalNodes;
         Map<String, GraphNode> nodesHelper = new TreeMap<String, GraphNode>();
@@ -108,34 +116,82 @@ public class ApplicationController {
 
         GraphNode source = GraphProcesser.firstNode(nodeList);
         gp = new GraphProcesser();
-        GraphProcesser.calculateShortestPathInDirectedGraph(nodeList.get(0));
-        criticalNodes = gp.findCriticalNodes(nodeList.get(0),nodeList,GraphProcesser.lastNode(nodeList));
-        for(GraphNode n : GraphProcesser.lastNode(nodeList).getShortestPath()) {
-            graph.getNode(n.getName()).setAttribute("ui.style",
-                    "fill-color: green; " );
-            shortestPath += n.getName() + " ";
-        }
-        shortestPath += GraphProcesser.lastNode(nodeList).getName();
-        graph.getNode(GraphProcesser.lastNode(nodeList).getName()).setAttribute("ui.style",
-                "fill-color: green; " );
 
-        for(GraphNode n : criticalNodes) {
-            if(n != GraphProcesser.lastNode(nodeList) && n.getNeighbours().size() == 1) {
-                String nextCriticalNode = n.getName()
-                        + " " + n.getNeighbours().entrySet().iterator().next().getValue() + " "
-                        + n.getNeighbours().entrySet().iterator().next().getKey().getName();
-                criticalNodesText+=nextCriticalNode + ", ";
-
-                graph.getEdge(nextCriticalNode).setAttribute("ui.style",
-                        "text-background-color: red; " );
+        if(gp.graphAcyclic(nodeList)) {
+            GraphProcesser.calculateShortestPathInDirectedGraph(nodeList.get(0));
+            criticalNodes = gp.findCriticalNodes(nodeList.get(0), nodeList, GraphProcesser.lastNode(nodeList));
+            for (GraphNode n : GraphProcesser.lastNode(nodeList).getShortestPath()) {
+                graph.getNode(n.getName()).setAttribute("ui.style",
+                        "fill-color: green; ");
+                shortestPath += n.getName() + " ";
             }
+            shortestPath += GraphProcesser.lastNode(nodeList).getName();
+            graph.getNode(GraphProcesser.lastNode(nodeList).getName()).setAttribute("ui.style",
+                    "fill-color: green; ");
+
+            for (GraphNode n : criticalNodes) {
+                if (n != GraphProcesser.lastNode(nodeList) && n.getNeighbours().size() == 1) {
+                    String nextCriticalNode = n.getName()
+                            + " " + n.getNeighbours().entrySet().iterator().next().getValue() + " "
+                            + n.getNeighbours().entrySet().iterator().next().getKey().getName();
+                    criticalNodesText += nextCriticalNode + ", ";
+
+                    graph.getEdge(nextCriticalNode).setAttribute("ui.style",
+                            "text-background-color: red; ");
+                }
+            }
+            if (criticalNodesText == "") criticalNodesText = "No critical activities";
+            theShortestPathOutput.setText(shortestPath);
+
+
+            criticalNodesOutput.setText(criticalNodesText);
+        } else {
+            warning.setVisible(true);
+            criticalNodesOutput.setText("");
+            theShortestPathOutput.setText("");
         }
-        if(criticalNodesText == "") criticalNodesText = "No critical nodes";
-        theShortestPathOutput.setText(shortestPath);
-        criticalNodesOutput.setText(criticalNodesText);
+
+        if(GraphProcesser.validateNecessaryActivitiesTextField(necessaryNodes, nodeList)) {
+            ArrayList<GraphNode> nodeTempList = new ArrayList<GraphNode>(nodeList);
+            LinkedHashMap<GraphNode, GraphNode> neededNodesWithNeighbours = GraphProcesser.getListOfNecessaryNodes(necessaryNodes, nodeTempList);
+            for(Map.Entry<GraphNode,GraphNode> node: neededNodesWithNeighbours.entrySet()) {
+                System.out.println("neededNodesWithNeighbours NODE :: " + node.getKey().getName());
+            }
+            ArrayList<GraphNode> shortestPathWithNecessaryNodes = new ArrayList<>();
+
+            for(Map.Entry<GraphNode, GraphNode> nodeGraphNodeEntry: neededNodesWithNeighbours.entrySet()) {
+                nodeTempList = new ArrayList<>(nodeList);
+                shortestPathWithNecessaryNodes.addAll(nodeGraphNodeEntry.getKey().getShortestPath());
+                shortestPathWithNecessaryNodes.add(nodeGraphNodeEntry.getKey());
+                //shortestPathWithNecessaryNodes.add(nodeGraphNodeEntry.getValue());
+                for(GraphNode node: nodeList) {
+                    node.clearShortestPath();
+                    node.setDistance(Integer.MAX_VALUE);
+                }
+                GraphProcesser.calculateShortestPathInDirectedGraph(nodeGraphNodeEntry.getValue());
+                for(GraphNode node: GraphProcesser.lastNode(nodeList).getShortestPath()) {
+                    System.out.println("PATH ::::: " + node.getName());
+                }
+            }
+
+            for(GraphNode node: nodeList) {
+                node.clearShortestPath();
+                node.setDistance(Integer.MAX_VALUE);
+            }
+            Map.Entry<GraphNode, GraphNode> lastMapEntry = (Map.Entry<GraphNode, GraphNode>) neededNodesWithNeighbours.entrySet().toArray()[neededNodesWithNeighbours.size() -1];
+            GraphProcesser.calculateShortestPathInDirectedGraph(lastMapEntry.getValue());
+            shortestPathWithNecessaryNodes.addAll(GraphProcesser.lastNode(nodeList).getShortestPath());
+            shortestPathWithNecessaryNodes.add(GraphProcesser.lastNode(nodeList));
+
+            for(GraphNode node: shortestPathWithNecessaryNodes) {
+                shortestPathWithNecessaryActivitiesOutput.setText(shortestPathWithNecessaryActivitiesOutput.getText() + " " + node.getName());
+            }
+        } else {
+            if(necessaryNodes.getText().length() > 1) necessaryNodesWarning.setVisible(true);
+        }
     }
 
-    public boolean areRowsValidated(String[] rows) {
+    public static boolean areRowsValidated(String[] rows) {
         boolean validated = true;
         Pattern letterPattern = Pattern.compile("[a-zA-Z]*");
         Pattern numberPattern = Pattern.compile("[0-9]*");
